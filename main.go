d9ef1d25ff265eb91e6cd46c4c0d1a1224a70dbc
mlabouardy <mohamed@labouardy.com>
2017-04-16 11:54:38 +0200

    create new content

---
+++ main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bytes"
+	b64 "encoding/base64"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/BurntSushi/toml"
+)
+
+type Commit struct {
+	Message   string    `json:"message"`
+	Committer Committer `json:"committer"`
+	Content   string    `json:"content"`
+	Path      string    `json:"path"`
+}
+
+type Committer struct {
+	Name  string `json:"name"`
+	Email string `json:"email"`
+}
+
+type Config struct {
+	Username    string `toml:"username"`
+	Email       string `toml:"email"`
+	Repository  string `toml:"repository"`
+	Message     string `toml:"message"`
+	Content     string `toml:"content"`
+	AccessToken string `toml:"access_token"`
+	Directory   string `toml:"directory"`
+}
+
+func main() {
+
+	config := Config{}
+	data, err := ioutil.ReadFile("config.conf")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(string(data))
+	_, _err := toml.Decode(string(data), &config)
+
+	if _err != nil {
+		log.Fatal(_err)
+	}
+
+	filename := fmt.Sprintf("LOG-%s.log", time.Now().Format("20060102150405"))
+	path := config.Directory + "/" + filename
+	url := fmt.Sprintf("http://api.github.com/repos/%s/%s/contents/%s?access_token=%s", config.Username, config.Repository, path, config.AccessToken)
+
+	commit := Commit{
+		Message: config.Message,
+		Committer: {
+			Name:  config.Username,
+			Email: config.Email,
+		},
+		Content: b64.StdEncoding.EncodeToString([]byte(config.Content)),
+		Path:    path,
+	}
+
+	client := http.Client{}
+
+	body := new(bytes.Buffer)
+	json.NewEncoder(body).Encode(commit)
+
+	req, err := http.NewRequest("PUT", url, body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(resp)
+}
