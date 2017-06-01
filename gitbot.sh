2d028d09b3a92ea44560d418f73f95b84cd01d30
LABOUARDY Mohamed <mohamed.labouardy@gmail.com>
2017-06-01 15:15:18 +0200

    Update gitbot.sh

--- gitbot.sh
+++ gitbot.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 content=$(echo -ne "hello world" | base64)
-token="<TOKEN>"
-username="<USERNAME>"
-repository="<REPOSITORY>"
+token="$1"
+username="$2
+repository="$3
 timestamp=$(date +%s)
 
 curl -vi -H "Accept: application/json" -H "Content-Type: application/json" -X PUT -d '{"content":"'$content'", "message":"random message"}' https://api.github.com/repos/$username/$repository/contents/output/LOG-$timestamp.log?access_token=$token
